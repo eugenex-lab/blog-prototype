23978fde997216e25e6158d15d094cc826b3a233
eugene kolapo opeyemi <kolapo.eugene@gmail.com>
2023-05-01 15:37:12 +0100

    feats 1.save data to local storage 4.

--- src/store/index.js
+++ src/store/index.js
@@ -126,13 +126,30 @@ export default createStore({
 
         async FETCH_DATA_LIST({commit}) {
 
+
+            // declara variable for local storage
+            let storageDate = localStorage.getItem('myData')
+
+            // check if local storage is empty
+            if (storageDate) {
+                console.log('data from local storage')
+                // if not empty, parse the data and set it to the state
+                commit('ADD_DATA_LIST', JSON.parse(storageDate))
+                // set showLoadStateBigBigArticle to false
+                commit('SET_STATUS_ALERT_WARNING', false)
+            }
+            else {
             await axios.get(`https://techcrunch.com/wp-json/wp/v2/posts?per_page=10&per_page=30`)
                 .then(response => {
                         console.log(response.data)
                         commit('ADD_DATA_LIST', response.data)
                         // set showLoadStateBigBigArticle to false
                     commit('SET_STATUS_ALERT_WARNING', false)
+                    localStorage.setItem('myData', JSON.stringify(response.data))
+
+
                     // commit('SET_SHOW_STATUS_ALERT', true   )
+                    console.log('data from api')
                     }
                 )
                 .catch(error => {
@@ -142,10 +159,13 @@ export default createStore({
                     commit('SET_SHOW_STATUS_ALERT', true   )
                     }
                 )
+        }
         },
 
 
         async FETCH_SINGLE_POST({commit}) {
+
+
 
             (await axios.get("https://techcrunch.com/wp-json/wp/v2/posts?per_page=10&per_page=15")
                     .then(response => {
